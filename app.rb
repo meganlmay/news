6cdf978a2979d76fdd2d8414526e529f1196d5c7
meganlmay <megan.may@kellogg.northwestern.edu>
2020-02-22 20:48:43 +0000

    assignment

--- app.rb
+++ app.rb
@@ -9,11 +9,31 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "8553542a76795ffd115abd7a4c012a1e"
 
+# News API
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0007a9bfcdcf4108bd2f99b97bd7b655"
+    news = HTTParty.get(url).parsed_response.to_hash
+
 get "/" do
-  # lat: ± 90.0
-  # long: ± 180.0
-  @lat = rand(-90.0..90.0)
-  @long = rand(-180.0..180.0)
-  @lat_long = "#{@lat},#{@long}"
-  view "geocode"
+  # show a view that asks for the location
+  view "ask"
+end
+
+get "/news" do
+  # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates #=> [lat, long]
+    location = results.first.city
+  # Define the lat and long
+    @lat = "#{lat_long [0]}"
+    @long = "#{lat_long [1]}"
+    @lat_long = "#{@lat},#{@long}"
+  # Results from Geocoder
+    forecast = ForecastIO.forecast("#{lat_long [0]}","#{lat_long[1]}").to_hash
+    current_temperature = forecast["currently"]["temperature"]
+    conditions = forecast["currently"]["summary"]
+  # high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+  # puts high_temperature
+  # puts forecast["daily"]["data"][1]["temperatureHigh"]
+  # puts forecast["daily"]["data"][2]["temperatureHigh"]
+    view "news"
 end
