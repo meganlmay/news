f86ae89afaf620b488c00589120e18ac02e890e4
meganlmay <megan.may@kellogg.northwestern.edu>
2020-02-24 04:38:27 +0000

    Final Megan

--- app.rb
+++ app.rb
@@ -18,8 +18,8 @@ get "/news" do
   # do everything else
 
 results = Geocoder.search(params["q"])
-    @lat_long = results.first.coordinates # => [lat, long]
-    @location = results.first.city
+@lat_long = results.first.coordinates # => [lat, long]
+@location = results.first.city
 
 # Define the lat and long
 @lat = "#{@lat_long [0]}"
@@ -27,34 +27,30 @@ results = Geocoder.search(params["q"])
 
 # Results from Geocoder
 @forecast = ForecastIO.forecast("#{@lat}" , "#{@long}").to_hash
-
 @current_temperature = @forecast["currently"]["temperature"]
 @current_conditions = @forecast["currently"]["summary"]
-
-# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
-# puts high_temperature
-# puts forecast["daily"]["data"][1]["temperatureHigh"]
-# puts forecast["daily"]["data"][2]["temperatureHigh"]
-
+  
   @daily_temperature = []
   @daily_conditions = []
   @daily_wind = []
   @daily_humidity = []
-
+  @daily_icon = []
+  
 for @day_forecast in @forecast["daily"]["data"] do
   @daily_temperature << @day_forecast["temperatureHigh"]
   @daily_conditions << @day_forecast["summary"]
   @daily_wind << @day_forecast["windSpeed"]
-  @daily_humidity << @day_forecast["humidity"]
+  @daily_humidity << @day_forecast["humidity"] 
+  @daily_icon << @day_forecast["icon"]
 end
-@list = @daily_temperature, @daily_conditions, @daily_wind, @daily_humidity
+@list = @daily_temperature, @daily_conditions, @daily_wind, @daily_humidity, @daily_icons
 
 # News API
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0007a9bfcdcf4108bd2f99b97bd7b655"
 @news = HTTParty.get(url).parsed_response.to_hash
 
-  @news_title = []
-  @story_url = []
+ @news_title = []
+ @story_url = []
 
 for daily_news in @news["articles"] do
   @news_title << daily_news["title"]
