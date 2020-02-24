32d9217ed7e5f25a521effbae4bbc5df53021cc9
meganlmay <megan.may@kellogg.northwestern.edu>
2020-02-24 00:15:57 +0000

    assignment final

--- app.rb
+++ app.rb
@@ -15,19 +15,54 @@ get "/" do
 end
 
 get "/news" do
-    results = Geocoder.search(params["q"])
-    @location = params["q"]
-  # News API
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0007a9bfcdcf4108bd2f99b97bd7b655"
-    news = HTTParty.get(url).parsed_response.to_hash
-  # Define the lat and long
-    lat_long = results.first.coordinates # => [lat, long]
-    lat = "#{lat_long [0]}"
-    long = "#{lat_long [1]}"
-    forecast = ForecastIO.forecast("#{lat}","#{long}").to_hash
-    @current_temperature = forecast["currently"]["temperature"]
-    @conditions = forecast["currently"]["summary"]
-    @forecast = forecast ["daily"]["data"]
-    @paper = news ["articles"] 
-    view "news"
+  # do everything else
+
+results = Geocoder.search(params["q"])
+    @lat_long = results.first.coordinates # => [lat, long]
+    @location = results.first.city
+
+# Define the lat and long
+@lat = "#{@lat_long [0]}"
+@long = "#{@lat_long [1]}"
+
+# Results from Geocoder
+@forecast = ForecastIO.forecast("#{@lat}" , "#{@long}").to_hash
+
+@current_temperature = @forecast["currently"]["temperature"]
+@current_conditions = @forecast["currently"]["summary"]
+
+# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+# puts high_temperature
+# puts forecast["daily"]["data"][1]["temperatureHigh"]
+# puts forecast["daily"]["data"][2]["temperatureHigh"]
+
+  @daily_temperature = []
+  @daily_conditions = []
+  @daily_wind = []
+  @daily_humidity = []
+
+for @day_forecast in @forecast["daily"]["data"] do
+  @daily_temperature << @day_forecast["temperatureHigh"]
+  @daily_conditions << @day_forecast["summary"]
+  @daily_wind << @day_forecast["windSpeed"]
+  @daily_humidity << @day_forecast["humidity"]
+end
+@list = @daily_temperature, @daily_conditions, @daily_wind, @daily_humidity
+
+# News API
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0007a9bfcdcf4108bd2f99b97bd7b655"
+@news = HTTParty.get(url).parsed_response.to_hash
+
+  @news_title = []
+  @story_url = []
+
+for daily_news in @news["articles"] do
+  @news_title << daily_news["title"]
+  @story_url << daily_news["url"]
+  
+end
+@newslist = @news_title, @story_url
+
+view "news"
+
 end
